f96f3f20367c9247e39417e24181e5a7d9ad822e
dimarezznik <stegalek@mail.ru>
2022-04-12 09:48:16 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './style.css'
+import IMAGE from './someImg.png'
+
+export const App = () => {
+    return <div className='middle'>
+        <h1>React ts webpack - {process.env.NODE_ENV}</h1>
+        <img src={IMAGE} alt="2312"/>
+    </div>
+}
