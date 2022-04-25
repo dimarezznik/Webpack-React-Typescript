2400f790443e2b1b28ed0386f1d33b752b0cee7f
dimarezznik <stegalek@mail.ru>
2022-04-25 11:37:32 +0300

    избавление от варнингов

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import './style.css'
-import IMAGE from './someImg.png'
+import "./style.css";
+import IMAGE from "./someImg.png";
 
 export const App = () => {
-    return <div className='middle'>
-        <h1>React ts webpack - {process.env.NODE_ENV}</h1>
-        <img src={IMAGE} alt="2312"/>
+  return (
+    <div className="middle">
+      <h1>React ts webpack - {process.env.NODE_ENV}</h1>
+      <img src={IMAGE} alt="2312" />
     </div>
-}
+  );
+};
